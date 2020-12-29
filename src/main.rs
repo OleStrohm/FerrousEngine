56a4a6022d1aa73a5070d485da32876c5b247615
OleStrohm <strohm99@gmail.com>
2020-12-29 12:47:56 +0000

    Triangle is now being drawn!

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,8 @@ use glfw::Context;
 use input_handler::InputHandler;
 
 fn main() {
+    println!("Welcome to a rusty engine!");
+
     let mut glfw = glfw::init(glfw::FAIL_ON_ERRORS).unwrap();
 
     glfw.window_hint(glfw::WindowHint::Resizable(false));
@@ -34,16 +36,73 @@ fn main() {
         render::Shader::from_vert_source(&CString::new(include_str!("triangle.vert")).unwrap())
             .unwrap();
     let frag_shader =
-        render::Shader::from_vert_source(&CString::new(include_str!("triangle.frag")).unwrap())
+        render::Shader::from_frag_source(&CString::new(include_str!("triangle.frag")).unwrap())
             .unwrap();
 
     let shader_program = render::Program::from_shaders(&[vert_shader, frag_shader]).unwrap();
 
     shader_program.set_used();
 
+    let vertices: Vec<f32> = vec![
+        -0.5, -0.5, 0.0, 1.0, 0.0, 0.0,
+         0.5, -0.5, 0.0, 0.0, 1.0, 0.0,
+         0.0,  0.5, 0.0, 0.0, 0.0, 1.0,
+    ];
+
+    let mut vbo: gl::types::GLuint = 0;
+    unsafe {
+        gl::GenBuffers(1, &mut vbo);
+
+        gl::BindBuffer(gl::ARRAY_BUFFER, vbo);
+        gl::BufferData(
+            gl::ARRAY_BUFFER,
+            (vertices.len() * std::mem::size_of::<f32>()) as gl::types::GLsizeiptr,
+            vertices.as_ptr() as *const gl::types::GLvoid,
+            gl::STATIC_DRAW,
+        );
+        gl::BindBuffer(gl::ARRAY_BUFFER, 0);
+    }
+
+    let mut vao: gl::types::GLuint = 0;
+    unsafe {
+        gl::GenVertexArrays(1, &mut vao);
+        gl::BindVertexArray(vao);
+
+        gl::BindBuffer(gl::ARRAY_BUFFER, vbo);
+
+        gl::EnableVertexAttribArray(0);
+        gl::VertexAttribPointer(
+            0,
+            3,
+            gl::FLOAT,
+            gl::FALSE,
+            (6 * std::mem::size_of::<f32>()) as gl::types::GLint,
+            std::ptr::null(),
+        );
+
+        gl::EnableVertexAttribArray(1);
+        gl::VertexAttribPointer(
+            1,
+            3,
+            gl::FLOAT,
+            gl::FALSE,
+            (6 * std::mem::size_of::<f32>()) as gl::types::GLint,
+            (3 * std::mem::size_of::<f32>()) as *const gl::types::GLvoid,
+        );
+
+        gl::BindBuffer(gl::ARRAY_BUFFER, 0);
+        gl::BindVertexArray(0);
+    }
+
     while !window.should_close() {
         unsafe {
             gl::Clear(gl::COLOR_BUFFER_BIT);
+        }
+
+        shader_program.set_used();
+        unsafe {
+            gl::BindVertexArray(vao);
+            gl::DrawArrays(gl::TRIANGLES, 0, 3);
         }
 
         window.swap_buffers();
